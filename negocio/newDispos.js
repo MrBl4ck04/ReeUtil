a494b3602bcf5eef796fac499f1e8851b63cc991
Talitos7 <carlocaba2004@gmail.com>
2024-09-12 14:09:18 -0400

    Quitadode archivos innecesarios

--- negocio/newDispos.js
+++ negocio/newDispos.js
@@ -1,27 +1,23 @@
 const mongoose = require('mongoose');
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 
-// Definir el esquema para dispositivos
 const dispositivoSchema = new mongoose.Schema({
-  idCatalogo: Number, // Este campo será autoincrementable
+  idCatalogo: Number, 
   descripcion: String,
-  idUsuario: { type: Number, default: null }, // Por defecto, será null
+  idUsuario: { type: Number, default: null }, 
   imagenProdu: String,
   marca: String,
   modelo: String,
   nombre: String,
   tipo: String
 }, {
-  versionKey: false // Eliminar el campo __v
+  versionKey: false 
 });
 
-// Añadir autoincremento a idCatalogo
 dispositivoSchema.plugin(AutoIncrement, { inc_field: 'idCatalogo' });
 
-// Modelo basado en el esquema, con la colección 'catalogo'
 const Dispositivo = mongoose.model('Dispositivo', dispositivoSchema, 'catalogo');
 
-// Función para registrar un dispositivo
 const registrarDispositivo = async (req, res) => {
   const { descripcion, imagenProdu, marca, modelo, nombre, tipo } = req.body;
 
@@ -30,7 +26,6 @@ const registrarDispositivo = async (req, res) => {
   }
 
   try {
-    // Crear una nueva instancia del modelo
     const nuevoDispositivo = new Dispositivo({
       descripcion,
       imagenProdu,
@@ -42,7 +37,6 @@ const registrarDispositivo = async (req, res) => {
 
     console.log('Datos que se intentan guardar:', nuevoDispositivo);
 
-    // Guardar el dispositivo en la base de datos
     const resultado = await nuevoDispositivo.save();
     console.log('Dispositivo guardado en la base de datos:', resultado);
 
