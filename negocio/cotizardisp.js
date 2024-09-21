bd81a286bc3734ec312f62678407710dea115448
BleeesJc <matevalen.xd@gmail.com>
2024-09-20 22:45:33 -0400

    Arreglos de cotizar dispositivo

--- negocio/cotizardisp.js
+++ negocio/cotizardisp.js
@@ -4,9 +4,9 @@ const AutoIncrement = require('mongoose-sequence')(mongoose);
 const cotizarSchema = new mongoose.Schema({
     cotizacion: { type: Number, default: null }, 
     detalles: String,
-    estadoCotizaci: { type: String, default: 'Pendiente'}, 
+    estadoCotizaci: { type: String, default: 'En Curso'}, 
     estadoDisposit: String,
-    fecha: Date, 
+    fecha: { type: Date, default: Date.now },
     idCatalogo: { type: Number, default: null }, 
     idUsuario: { type: Number, default: null }, 
     imagen: String 
@@ -17,22 +17,22 @@ const cotizarSchema = new mongoose.Schema({
 cotizarSchema.plugin(AutoIncrement, { inc_field: 'idDispositivo' });
 
 const Cotizacion = mongoose.model('dispositivos', cotizarSchema);
+
 const env = async (req, res) => {
     console.log('Datos recibidos en el servidor:', req.body);
   
-    const { detalles, estado, fecha, idCatalogo,idUsuario} = req.body;
+    const { detalles, estado, idCatalogo, idUsuario } = req.body;
   
-    if (!detalles || !estado || !fecha) {
+    if (!detalles || !estado) {
       console.log('Faltan datos obligatorios');
       return res.status(400).json({ error: 'Faltan datos obligatorios' });
     }
   
     try {
-
       const enviarcot = new Cotizacion({
         detalles: detalles,
         estadoDisposit: estado,
-        fecha: new Date(fecha),
+        fecha: new Date(),
         idCatalogo: idCatalogo,
         idUsuario: idUsuario,
         imagen: 'sadas' 
@@ -47,7 +47,8 @@ const env = async (req, res) => {
       console.error('Error al guardar en MongoDB:', err);
       res.status(500).json({ error: `Error al registrar el dispositivo: ${err.message}` });
     }
-  };
+};
+
 module.exports = {
   env
 };
